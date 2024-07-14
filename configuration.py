27f8bec19c8ca628193094ee1dabebb9659fdacb
JKwef <vanek2002002@gmail.com>
2024-07-14 18:43:07 +0300

    2

---
+++ configuration.py
@@ -0,0 +1,4 @@
+# Козубенко Иван, 18 когорта — Финальный проект. Инженер по тестированию плюс
+URL_SERVICE = "https://397078a9-d1fd-47ab-bf2e-c642c61961f5.serverhub.praktikum-services.ru/"
+CREATE_ORDER = "/api/v1/orders"
+GET_ORDER = "/api/v1/orders/track"
